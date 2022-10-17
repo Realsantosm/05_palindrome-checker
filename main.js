61baad8178266144f040a929f305bc91cb128422
Santos <realsantosm@gmail.com>
2022-10-17 22:27:42 +0530

    reafctor js file

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
 var checkButton = document.getElementById("checkPalindrome");
-console.log(checkButton);
+// console.log(checkButton); Debugger
 
-
-function isPalindrome(str) {  
+function isPalindrome(str) {
   str = str.replace(/[^a-z0-9]+/gi, "").toLowerCase();
   var reversedStr = str.split("").reverse().join("");
   return str === reversedStr;
 }
-
-checkButton.addEventListener("click", function() {  
+// Adding event
+checkButton.addEventListener("click", function () {
   var value = document.getElementById("inputPalindrome").value;
-  var notification = document.getElementById("notification");  
-  if(isPalindrome(value)) {
+  var notification = document.getElementById("notification");
+  if (isPalindrome(value)) {
     notification.innerHTML = "Yay! You've got yourself a palindrome";
     notification.className = "alert alert-success";
   } else {
